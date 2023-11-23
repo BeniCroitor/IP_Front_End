56f731c997bb74f0647a79cb116b36d50060d73c
BeniCroitor <101263996+BeniCroitor@users.noreply.github.com>
2023-11-23 12:33:15 +0200

    Initial Setup

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>8 KRAY</h1>
+      <p>Lorem ipsum dolor sit amet sdksjhfks;d</p>
+    </div>
+  );
+};
+
+export default Home;

9e3ee0aceefdfee520f45a00fe5bb849e532b0fc
VictorTatarciuc <victor.tatarciuc@student.usv.ro>
2023-11-23 17:23:23 +0200

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,7 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
   },
+  rules: {
+    'linebreak-style': ['error', 'unix', 'windows'],
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+/*eslint-disable*/
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import './App.css';
 
